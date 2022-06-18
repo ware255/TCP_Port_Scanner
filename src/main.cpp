5d22690dc9aacd10b73bb2b672d6856393a7e41f
われ <74850252+ware255@users.noreply.github.com>
2022-06-18 22:15:56 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+/*
+ * 参考にしたもの
+ * http://capm-network.com/?tag=C%E8%A8%80%E8%AA%9E-%E3%82%BD%E3%82%B1%E3%83%83%E3%83%88%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0_TCP%E3%83%9D%E3%83%BC%E3%83%88%E3%82%B9%E3%82%AD%E3%83%A3%E3%83%B3
+ */
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
